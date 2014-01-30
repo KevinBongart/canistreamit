a8839f97dc68659a14c3ea343cc78b94c9c906d8
Kevin Bongart <contact@kevinbongart.net>
2014-01-30 16:44:32 -0500

    Search movies and query CISI

--- lib/canistreamit/client.rb
+++ lib/canistreamit/client.rb
@@ -1,7 +1,13 @@
+require "httparty"
+
 module Canistreamit
   class Client
+    include HTTParty
+
     # Define the same set of accessors as the Canistreamit module
     attr_accessor *Configuration::VALID_CONFIG_KEYS
+
+    base_uri Configuration::DEFAULT_ENDPOINT
 
     def initialize(options={})
       # Merge the config values from the module and those passed
@@ -13,6 +19,21 @@ module Canistreamit
       Configuration::VALID_CONFIG_KEYS.each do |key|
         send("#{key}=", merged_options[key])
       end
+    end
+
+    def search(movie_name)
+      response = self.class.get "/search", query: { movieName: movie_name }
+      response.parsed_response
+    end
+
+    def query(movie_id, media_type)
+      response = self.class.get "/query", query: {
+        movieId: movie_id,
+        attributes: '1',
+        mediaType: media_type
+      }
+
+      response.parsed_response
     end
   end
 end
